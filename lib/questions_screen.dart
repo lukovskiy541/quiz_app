395183a983262b17515145fc44c4d95849c52970
Max Lukovskiy <ragekrimson@gmail.com>
2024-11-16 16:20:33 +0200

    animations

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -13,14 +13,53 @@ class QuestionsScreen extends StatefulWidget {
   }
 }
 
-class _QuestionsScreenState extends State<QuestionsScreen> {
+class _QuestionsScreenState extends State<QuestionsScreen> with SingleTickerProviderStateMixin{
   var currentQuestionIndex = 0;
+  late AnimationController _animationController;
+  late Animation<Offset> _slideAnimation;
+  late Animation<double> _opacityAnimation;
 
-  void answerQuestion(String selectedAnswer) {
+  void answerQuestion(String selectedAnswer) async  {
+    await _animationController.reverse();
     setState(() {
       currentQuestionIndex++;
       widget.onSelectAnswer(selectedAnswer);
     });
+    if (currentQuestionIndex < questions.length) {
+      _animationController.forward();
+    }
+  }
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      duration: const Duration(milliseconds: 300),
+      vsync: this,
+    );
+
+    _slideAnimation = Tween<Offset>(
+      begin: const Offset(1.0, 0.0),
+      end: const Offset(0.0, 0.0),
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeInOut,
+    ));
+
+    _opacityAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeIn,
+    ));
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
@@ -30,30 +69,35 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
       width: double.infinity,
       child: Container(
         margin: const EdgeInsets.all(40),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              currentQuestion.text,
-              style: GoogleFonts.notoSans(
-                  color: const Color.fromARGB(255, 201, 153, 251),
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
+        child: SlideTransition(
+          position: _slideAnimation,
+          child: FadeTransition(
+            opacity: _opacityAnimation,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  currentQuestion.text,
+                  style: GoogleFonts.notoSans(
+                    color: const Color.fromARGB(255, 201, 153, 251),
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 30),
+                ...currentQuestion.getShuffledAnswers().map((item) {
+                  return AnswerButton(
+                    answerText: item,
+                    onTap: () {
+                      answerQuestion(item);
+                    },
+                  );
+                }),
+              ],
             ),
-            const SizedBox(
-              height: 30,
-            ),
-            ...currentQuestion.getShuffledAnswers().map((item) {
-              return AnswerButton(
-                answerText: item,
-                onTap: () {
-                  answerQuestion(item);
-                },
-              );
-            })
-          ],
+          ),
         ),
       ),
     );
